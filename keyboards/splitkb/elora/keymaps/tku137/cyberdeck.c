656f534948c3c421ec4b6f5b7e3a570eef39d45f
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-03-29 15:09:34 +0100

    cleanup

--- keyboards/splitkb/elora/keymaps/tku137/cyberdeck.c
+++ keyboards/splitkb/elora/keymaps/tku137/cyberdeck.c
@@ -111,21 +111,6 @@ void render_cyberdeck(void) {
     oled_write_raw_P(tku137, sizeof(tku137));
 }
 
-// Function to blink specified pixel
-static uint32_t last_blink_time = 0;
-static bool     blink_state     = false;
-
-void blink_pixel(uint8_t x, uint8_t y) {
-    // Check if it's time to toggle the blink state
-    if (timer_elapsed32(last_blink_time) >= BLINK_INTERVAL) {
-        last_blink_time = timer_read32(); // Reset the timer
-        blink_state     = !blink_state;   // Toggle the state
-
-        // Toggle the state of the single pixel
-        oled_write_pixel(x, y, blink_state);
-    }
-}
-
 // WPM-responsive animation rendering
 
 // Draw a column on the OLED buffer
@@ -210,6 +195,13 @@ void render_wpm_columns_animation(void) {
 // These functions are called in the `oled_task_user` function
 // They are meant as an interface to minimize maintenance when switching to
 // other oled libraries or display types
+
+// Global states used in renders
+// For pixel blinking effect
+static uint32_t last_blink_time = 0;
+static bool     blink_state     = false;
+
+// Render the master OLED
 void render_master(void) {
     // show cyberdeck visualisation
     render_cyberdeck();
@@ -226,6 +218,7 @@ void render_master(void) {
     oled_render();
 }
 
+// Render the slave OLED
 void render_slave(void) {
     // render the WPM columns animation
     render_wpm_columns_animation();

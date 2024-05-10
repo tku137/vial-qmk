b3cce75c8c2a8ec0e78f1f7d51608d6ccef43e89
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-05-10 23:32:39 +0200

    keymap modifications

--- keyboards/splitkb/elora/keymaps/tku137/keymap.c
+++ keyboards/splitkb/elora/keymaps/tku137/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------+------|  |------|------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |  (   |  |   )  |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt/| Space| Nav  |  | Sym  | Bksp | AltGr| RGUI |F-Keys|
+ *                        |Adjust| Esc  | LAlt/| Space| Nav  |  | Sym  | Bksp | Tab  | RAlt |F-Keys|
  *                        |      |      | Enter|      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  *
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB  , KC_Q   ,  KC_W  ,  KC_E  ,   KC_R ,   KC_T ,          KC_LCTL,     KC_RCTL,           KC_Y  ,   KC_U ,   KC_I ,   KC_O ,   KC_P , KC_BSLS,
       CTL_ESC , H_CTL_A, H_ALT_S, H_SFT_D,H_GUI_F ,   KC_G ,          KC_LALT,     KC_RALT,           KC_H  , H_GUI_J, H_SFT_K, H_ALT_L,H_CTL_SC,CTL_QUOT,
       KC_LSFT , KC_Z   ,  KC_X  ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_LPRN,     KC_RPRN, KC_RBRC,  KC_N  ,   KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                  ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV    ,      SYM   , KC_BSPC, KC_RALT, KC_RGUI,  FKEYS ,
+                                  ADJUST , KC_ESC , ALT_ENT, KC_SPC , NAV    ,      SYM   , KC_BSPC, KC_TAB , KC_RALT,  FKEYS ,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
     ),
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------+------|  |------|------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   D  |   V  | [ {  |  (   |  |   )  |  ] } |   K  |   H  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt/| Space| Nav  |  | Sym  | Bksp | AltGr| RGUI |F-Keys|
+ *                        |Adjust| Esc  | LAlt/| Space| Nav  |  | Sym  | Bksp | Tab  | RAlt |F-Keys|
  *                        |      |      | Enter|      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  *
@@ -153,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB  ,  KC_Q  ,  KC_W  ,  KC_F  ,   KC_P ,   KC_B ,          KC_LCTL,     KC_RCTL,           KC_J  ,   KC_L ,   KC_U ,   KC_Y , KC_SCLN, KC_BSLS,
       CTL_ESC , H_CTL_A, H_ALT_R, H_SFT_S, H_GUI_T,   KC_G ,          KC_LALT,     KC_RALT,           KC_M  , H_GUI_N, H_SFT_E, H_ALT_I ,H_CTL_O ,CTL_QUOT,
       KC_LSFT ,  KC_Z  ,  KC_X  ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC, KC_LPRN,     KC_RPRN, KC_RBRC,  KC_K  ,   KC_H , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                  ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV    ,     SYM    , KC_BSPC, KC_RALT, KC_RGUI,  FKEYS ,
+                                  ADJUST , KC_ESC , ALT_ENT, KC_SPC , NAV    ,      SYM   , KC_BSPC, KC_TAB , KC_RALT,  FKEYS ,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
     ),

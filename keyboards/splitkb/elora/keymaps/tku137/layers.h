060513d0f0e3c1bd0256318b3bc0c0dacaa7e358
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-08-01 21:56:02 +0200

    implement dual gaming layers

--- keyboards/splitkb/elora/keymaps/tku137/layers.h
+++ keyboards/splitkb/elora/keymaps/tku137/layers.h
@@ -1,7 +1,6 @@
 #pragma once
 #include QMK_KEYBOARD_H
 
-
 enum layers {
     _QWERTY = 0,
     _COLEMAK_DH,
@@ -9,15 +8,23 @@ enum layers {
     _NAV,
     _FUNCTION,
     _ADJUST,
+    _GAMING,
+    _GAMING_EXT,
 };
 
-
 // Aliases for readability
-#define QWERTY   DF(_QWERTY)
-#define COLEMAK  DF(_COLEMAK_DH)
+// Typing layers
+#define QWERTY DF(_QWERTY)
+#define COLEMAK DF(_COLEMAK_DH)
 
-#define SYM      MO(_SYM)
-#define NAV      MO(_NAV)
-#define FKEYS    MO(_FUNCTION)
-#define ADJUST   MO(_ADJUST)
+// Extend layers
+#define SYM MO(_SYM)
+#define NAV MO(_NAV)
 
+// Function layers
+#define FKEYS MO(_FUNCTION)
+#define ADJUST MO(_ADJUST)
+
+// Special layers
+#define GAMING DF(_GAMING)
+#define GMEXT MO(_GAMING_EXT)
